f8ce2060068d53a24d72ea01088405af3ca43f8a
zacharyb12 <boualemzachary89@yahoo.fr>
2026-02-02 09:51:25 +0100

    tryparse

--- ExempleDemo/Program.cs
+++ ExempleDemo/Program.cs
@@ -1,10 +1,12 @@
 ﻿
+string valeurToConvert = Console.ReadLine();
 
-// hobby
-// vehicule
-// destination de vacances
-// salaire de rève
+bool isConverted = false;
 
 
-// afficher sur une ou plusieurs lignes un message pour présenter 
-// ces informations proprement
+int valeurConverted;
+
+isConverted = int.TryParse(valeurToConvert , out valeurConverted);
+
+Console.WriteLine(valeurConverted);
+
--- LesBasesDuCSharp/Program.cs
+++ LesBasesDuCSharp/Program.cs
@@ -193,6 +193,80 @@ int valeurNum = 54;
 
 string valeurNumString = valeurNum.ToString();
 
+//-----------------------------------------------------------------
+
+
+// Conversions
+
+// Vers String
+
+int valeurInt = 10;
+float valeurFloat2 = 3.14F;
+
+string valeurString2 = valeurInt.ToString();
+string valeurString3 = valeurFloat2.ToString();
+
+// -------
+
+// Vers type numerique
+
+// Parse : Type.Parse( paramètre à convertir );
+
+string valeurToConvert = "15";
+int valeurConverted = int.Parse(valeurToConvert);
+
+// -------
+
+// Try.Parse : renvoie un booléen , prend deux paramètres 
+// le premier la valeur à convertir
+// le deuxième dans quoi envoyer la valeur qui à été convertie ( out )
+
+
+string valeurToConvertTryParse = Console.ReadLine();
+
+bool isConverted = false;
+
+
+int valeurConvertedTryParse;
+
+isConverted = int.TryParse(valeurToConvertTryParse, out valeurConvertedTryParse);
+
+Console.WriteLine(valeurConverted);
+
+// -------
+
+// Convert
+
+string valeurToConvert2 = Console.ReadLine();
+
+int valueConverted = Convert.ToInt32(valeurToConvert2);
+
+// -------
+
+// Conversion implicite et explicite
+
+// implicite n'implique aucune perte de valeur
+int valeurIntImpl = 15;
+
+float valeurFloatImpl = valeurIntImpl;
+
+// explicite
+float valeurFloatToInt = 3.14F;
+
+int valeurIntFromFloat = (int)valeurFloatToInt;
+
+// ----------------------------------------------
+// boxing et unboxing
+
+int maValueInt = 10;
+
+// boxing
+
+Object o = maValueInt;
+
+// unboxing
+maValueInt = (int)o;
+
 
 
 
