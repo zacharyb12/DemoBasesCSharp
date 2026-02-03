cba04a4aed29facd5c3ae387bdcb048ff62bab8e
zacharyb12 <boualemzachary89@yahoo.fr>
2026-02-03 12:06:53 +0100

    demoExoCompte

--- ExempleDemo/Program.cs
+++ ExempleDemo/Program.cs
@@ -6,12 +6,23 @@
 //Modulo: 1,
 //Division: 2,5.
 
-//● Vérification d’un compte bancaire BBAN, si le compte est bon affichez OK sur la console
-//sinon KO.
-//Le modulo des 10 premiers chiffres par 97 donne les 2 derniers. Sauf si le modulo = 0 dans
-//ce cas les 2 derniers chiffres sont 97.
+
+// Vérification d’un compte bancaire BBAN,
+//si le compte est bon affichez OK sur la console sinon KO.
+
+//Le modulo des 10 premiers chiffres par 97 donne les 2 derniers.
+//Sauf si le modulo = 0 dans ce cas les 2 derniers chiffres sont 97.
 //(utilisez la méthode « Substring » de la classe « string »).
 
+// type numerique plus gros : long
+string texte = "voici un texte";
 
-//● Transformer un compte bancaire BBAN Belge (xxx-xxxxxxx-xx) en IBAN
+string result = texte.Substring(9); //à partir de ou selectionner le texte -> texte
+string result2 = texte.Substring(0,5); //à partir de ou et combien -> voici
+
+
+
+// Transformer un compte bancaire BBAN Belge (xxx-xxxxxxx-xx) en IBAN
 //(BExx-xxxx-xxxx-xxxx). Trouvez la démarche via un moteur de recherche.
+
+
