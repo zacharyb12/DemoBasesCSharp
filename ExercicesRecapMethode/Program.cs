ef94dd985fb3b7d61e9eb848b47bbb3b839239fa
zacharyb12 <boualemzachary89@yahoo.fr>
2026-02-05 15:06:26 +0100

    Exercices Mini Programme

--- ExercicesRecapMethode/Program.cs
+++ ExercicesRecapMethode/Program.cs
@@ -9,3 +9,9 @@
 // - verification IBAN
 // - table de multiplication
 // - verifie si l'année est bissextile
+
+// chaque Fonctionnalité sera implémenter dans une Procedure
+// les actions récurrentes seront dans des Fonctions ( getNumber )
+// le programme principale tourne dans une boucle 
+// laisser la possibilité a l'utilisateur de quitter le programme
+// Vous pouvez récupérer le code déja implementer lors des précédentes démos et exercices
